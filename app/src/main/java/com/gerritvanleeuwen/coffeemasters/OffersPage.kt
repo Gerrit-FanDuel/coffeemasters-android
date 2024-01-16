95afe695b699d30062f2bc303477bf0028057f6b
Gerrit-FanDuel <gerrit.vanleeuwen@fanduel.com>
2024-01-16 17:42:56 -0500

    Adding Background Image and Preview Composables

--- app/src/main/java/com/gerritvanleeuwen/coffeemasters/OffersPage.kt
+++ app/src/main/java/com/gerritvanleeuwen/coffeemasters/OffersPage.kt
@@ -1,51 +1,68 @@
 package com.gerritvanleeuwen.coffeemasters
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.gerritvanleeuwen.coffeemasters.ui.theme.Alternative1
 import com.gerritvanleeuwen.coffeemasters.ui.theme.Alternative2
 
-@Preview(showBackground = true, widthDp = 400)
+@Preview(showBackground = true)
 @Composable
-fun Offer() {
-    val style = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-
+fun OffersPage() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        Modifier.verticalScroll(rememberScrollState())
+    ) {
+        Offer(title = "Early Coffee", description = "10% off. Offer valid from 6am to 9am.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+        Offer(title = "Welcome Gift", description = "25% off on your first order.")
+    }
+}
+
+@Composable
+fun Offer(title: String, description: String) {
+    Box(
         modifier = Modifier.padding(16.dp)
     ) {
-        Text(text = "My Title",
-            style = MaterialTheme.typography.h5,
-            fontWeight = FontWeight.Bold,
+        Image(painter = painterResource(id = R.drawable.background_pattern),
+            contentDescription = "Background pattern",
+            contentScale = ContentScale.FillWidth,
             modifier = Modifier
-                .background(Alternative1)
-                .padding(16.dp)
+                .fillMaxWidth()
+                .height(180.dp)
+        )
 
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Description",
-            style = MaterialTheme.typography.h6,
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .background(Alternative2)
                 .padding(16.dp)
-        )
+                .fillMaxWidth()
+        ) {
+            Text(text = title,
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(Alternative1)
+                    .padding(16.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = description,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier
+                    .background(Alternative2)
+                    .padding(16.dp)
+            )
+        }
     }
 }
